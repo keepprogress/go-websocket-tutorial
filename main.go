b2d45af8421587bb9bf9952af304d4aa22f4f284
Nick.Hsu <Nick.Hsu@testritegroup.com>
2024-08-22 17:42:28 +0800

    init websocket tutorial

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+
+	// upgrade this connection to a WebSocket
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	// helpful Log statement to show connections
+	log.Println("Client Connected")
+
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		// reader in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
